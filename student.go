9d933d309398fe41c634ee5b95cb635430670779
Zhakeee <95651889+Zhakeee@users.noreply.github.com>
2023-01-31 16:48:22 +0600

    first

---
+++ student.go
@@ -0,0 +1,28 @@
+package main
+
+type Student struct {
+	FirstName  string
+	SecondName string
+	Age        int
+}
+
+func (s Student) GetFirstName() string {
+	return s.FirstName
+
+}
+func (s *Student) SetFirstName(firstname string) {
+	s.FirstName = firstname
+}
+func (s Student) GetSecondName() string {
+	return s.FirstName
+}
+func (s *Student) SetSecondName(secondname string) {
+	s.FirstName = secondname
+}
+func (s Student) GetAge() int {
+	return s.Age
+
+}
+func (s *Student) SetAge(age int) {
+	s.Age = age
+}
